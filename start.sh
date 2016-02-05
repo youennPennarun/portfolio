d63c212e6812ee9f8e6306672435164371fa0ce7
Youenn PENNARUN <youenn.pennarun@gmail.com>
2016-02-05 11:30:17 +0100

    start script

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo $UID
+BASEDIR=$(dirname $0)
+echo $BASEDIR
+if [ $(ps aux | grep -i "portfolio" | grep -v "grep" | wc -l) -eq 0 ]
+then
+	echo "starting portfolio"
+        export PATH=/usr/local/bin:$PATH
+	gulp build
+        forever start --sourceDir $BASEDIR $BASEDIR/server/app.js >> $BASEDIR/lo
+gs.txt
+else
+	echo "Already running"
+fi
